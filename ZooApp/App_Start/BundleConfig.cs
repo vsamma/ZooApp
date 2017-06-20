43c0ced98394f87fab04300db925de3e55c35593
Veiko Samma <veikosamma@gmail.com>
2017-06-20 15:50:08 +0300

    Animals listing done

--- ZooApp/App_Start/BundleConfig.cs
+++ ZooApp/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace ZooApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js"));
         }
     }
 }
---
+++ ZooApp/Scripts/app/animals-controller.js
@@ -0,0 +1,66 @@
+﻿(function () {
+
+    //Module 
+    var app = angular.module('ZooApp', []); 
+
+    // Controller
+    app.controller('AnimalsCtrl', function ($scope, $http) {
+        //$scope.answered = false;
+        $scope.title = "loading...";
+        $scope.message = "App is starting, eh?"
+        $scope.species = [];
+        $scope.animals = [];
+        //$scope.correctAnswer = false;
+        $scope.working = false;
+        $scope.avgAge = 0;
+
+        $scope.loadSpecies = function () {
+            $scope.working = true;
+            $scope.title = "loading species...";
+            $scope.species = [];
+
+
+            $http.get("/api/Species").then(function (response) {
+                //console.log("Species returned:");
+                //console.log(response);
+                $scope.species = response.data;
+                $scope.title = "Species loaded";
+                $scope.working = false;
+            }, function() {
+                $scope.title = "Oops... something went wrong with Species request";
+                $scope.working = false;
+            });
+        };
+
+        $scope.loadAnimals = function () {
+            $scope.working = true;
+            $scope.title = "loading animals...";
+            $scope.animals = [];
+
+            $http.get("/api/Animals").then(function (response) {
+                //console.log("Animals returned:");
+                //console.log(response);
+                $scope.animals = response.data;
+                $scope.title = "Animals loaded";
+                $scope.avgAge = $scope.calcAvgAge();
+                $scope.working = false;
+            }, function () {
+                $scope.title = "Oops... something went wrong with Animals request";
+                $scope.working = false;
+            });
+        };
+
+        
+
+
+        $scope.calcAvgAge = function () {
+            var sum = 0;
+
+            angular.forEach($scope.animals, function (value, key) {
+                sum += value.Age;
+            });
+            
+            return (sum / $scope.animals.length);
+        };
+    });
+})();
